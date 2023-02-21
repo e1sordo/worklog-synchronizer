ace8c7c2eeefc9e7071d792beddd09fd28f3ef32
e1sordo <vlavik007@gmail.com>
2023-02-21 14:05:57 +0300

    add the missing required OkHttpClient bean to make NotionClient work correctly

--- src/main/java/es/e1sordo/worklog_sync/config/NotionClientConfiguration.java
+++ src/main/java/es/e1sordo/worklog_sync/config/NotionClientConfiguration.java
@@ -1,6 +1,7 @@
 package es.e1sordo.worklog_sync.config;
 
 import feign.RequestInterceptor;
+import feign.okhttp.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +14,11 @@ public class NotionClientConfiguration extends RetryableAndLoggedGatewaysConfigu
     private static final String AUTHORIZATION_HEADER = HttpHeaders.AUTHORIZATION;
     private static final String NOTION_VERSION_HEADER = "Notion-Version";
     private static final String TOKEN_TYPE = "Bearer";
+
+    @Bean
+    public OkHttpClient client() {
+        return new OkHttpClient();
+    }
 
     @Bean
     public RequestInterceptor requestInterceptor(@Value("${notion.secret}") String secret) {
